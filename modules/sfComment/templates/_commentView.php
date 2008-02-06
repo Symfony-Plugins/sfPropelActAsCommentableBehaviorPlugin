5f007857ba1946b25818908fcf4e5943e1a7568f
xavier <xavier@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-02-06 12:05:59 -0600

    sfPropelActAsCommentableBehaviorPlugin: added anchors on comments, in order to ease up the access to their permalink
    git-svn-id: http://svn.symfony-project.com/plugins/sfPropelActAsCommentableBehaviorPlugin/trunk@7379 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/sfComment/templates/_commentView.php
+++ modules/sfComment/templates/_commentView.php
@@ -2,15 +2,15 @@
 <?php use_helper('Date'); ?>
 <div class="sf_comment" id="sf_comment_<?php echo $comment['Id'] ?>">
   <p class="sf_comment_info">
-    <span class="sf_comment_author">
+    <a href="#sf_comment_<?php echo $comment['Id'] ?>"><span class="sf_comment_author">
       <?php if (!is_null($comment['AuthorId'])): ?>
-        <?php 
-        include_component('sfComment', 
-                          'author', 
-                          array('author_id'    => $comment['AuthorId'], 
+        <?php
+        include_component('sfComment',
+                          'author',
+                          array('author_id'    => $comment['AuthorId'],
                                 'sf_cache_key' => $comment['AuthorId']));
         ?><?php else: ?><?php echo $comment['AuthorName'] ?><?php endif; ?></span>,
-    <?php echo __('%1% ago', array('%1%' => distance_of_time_in_words(strtotime($comment['CreatedAt'])))) ?>
+    <?php echo __('%1% ago', array('%1%' => distance_of_time_in_words(strtotime($comment['CreatedAt'])))) ?></a>
   </p>
   <p class="sf_comment_text">
     <?php echo $comment['Text']; ?>
